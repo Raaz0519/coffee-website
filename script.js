2531e833b42d5f63082f64f20121c101c2451c31
Raaz0519 <rajpraj0519@gmail.com>
2024-12-07 19:17:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const navbarLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+const searchBar = document.getElementById('searchBar');
+searchBar.addEventListener('input', (e) => {
+    const searchQuery = e.target.value.toLowerCase();
+    document.querySelectorAll('.menu-item').forEach(item => {
+        item.style.display = item.textContent.toLowerCase().includes(searchQuery) ? '' : 'none';
+    });
+});
+
+
+menuOpenButton.addEventListener("click", () => {
+  // Toggle mobile menu visibility
+  document.body.classList.toggle("show-mobile-menu");
+});
+
+// Close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Close menu when nav link is clicked
+navbarLinks.forEach((link) => {
+  link.addEventListener("click", () => menuOpenButton.click());
+});
+
+
+
+/* Initializing Swiper */
+let swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // Pagination bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  /* Responsive breakpoints */
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
